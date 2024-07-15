34d30e507c49ffc767b3911d9d250cae5d3f3de6
Muhammad Arfath <146614939+muhammedarfath@users.noreply.github.com>
2024-07-15 13:39:31 +0530

    change post details modal

--- src/Components/post/PostModal.js
+++ src/Components/post/PostModal.js
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
 import { Modal, ModalContent } from "@nextui-org/react";
-import { FaRegHeart } from "react-icons/fa";
-import { RiShare2Line } from "react-icons/ri";
-import { MdSaveAlt } from "react-icons/md";
-import { BsThreeDots } from "react-icons/bs";
-import Avatar_profile from "../avatar/Avatar_profile";
+import AvatarProfile from "../avatar/Avatar_profile";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { FcLike } from "react-icons/fc";
 import { AiOutlineHeart } from "react-icons/ai";
 import { TbMessageCircle } from "react-icons/tb";
-import { BsBookmark } from "react-icons/bs";
-import { BsBookmarkFill } from "react-icons/bs";
+import { RiShare2Line } from "react-icons/ri";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
 import LikeSVG from "./Likesvg";
+import { BsThreeDots } from "react-icons/bs";
 
 const PostModal = ({ isOpen, onClose, selectedPost }) => {
   const [likes, setLikes] = useState(selectedPost.likes);
@@ -21,10 +18,12 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
   const [fav, setFav] = useState(selectedPost.is_faved);
   const [showFullCaption, setShowFullCaption] = useState(false);
   const [showAllTags, setShowAllTags] = useState(false);
+
   const isImage = (mediaFile) => {
     const extension = mediaFile.split(".").pop().toLowerCase();
     return ["jpg", "jpeg", "png", "gif", "bmp"].includes(extension);
   };
+
   const toggleTags = (e) => {
     e.stopPropagation();
     setShowAllTags(!showAllTags);
@@ -39,9 +38,7 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
     try {
       const response = await axios.post(
         `http://127.0.0.1:8000/posts/like-post/${selectedPost.id}/`,
-        {
-          username: username,
-        }
+        { username: username }
       );
       if (response.data) {
         setLikes(response.data.post.likes);
@@ -54,13 +51,11 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
     }
   };
 
-  const handlefavorites = async (e) => {
+  const handlefavorites = async () => {
     try {
       const response = await axios.post(
         `http://127.0.0.1:8000/posts/fav-post/${selectedPost.id}/`,
-        {
-          username: username,
-        }
+        { username: username }
       );
       if (response.data) {
         setFav(!fav);
@@ -86,21 +81,15 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
   };
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      className="flex h-full lg:h-auto xl:auto md:h-auto sm:h-auto"
-      size="5xl"
-      hideCloseButton
-    >
+    <Modal isOpen={isOpen} onClose={onClose} className="flex h-screen" size="full">
       <ModalContent className="flex flex-row">
-        <div className="flex lg:w-1/2 flex-col bg-black">
+        <div className="flex w-full md:w-1/2 lg:w-1/2 flex-col bg-black">
           {selectedPost.media_file ? (
             isImage(selectedPost.media_file) ? (
               <img
                 src={`http://127.0.0.1:8000${selectedPost.media_file}`}
                 alt={`Image ${selectedPost.id}`}
-                className="w-full h-auto rounded-3xl"
+                className="w-full h-[90%] object-contain"
               />
             ) : (
               <video
@@ -108,21 +97,21 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
                 autoPlay
                 muted
                 src={`http://127.0.0.1:8000${selectedPost.media_file}`}
-                className="w-full h-auto rounded-3xl"
+                className="w-full h-[90%] object-contain"
               />
             )
           ) : (
             <img
               src={`http://127.0.0.1:8000${selectedPost.picture}`}
               alt={`Image ${selectedPost.id}`}
-              className="w-full h-auto rounded-3xl"
+              className="w-full h-[90%] object-contain"
             />
           )}
           <div className="w-full justify-center items-end">
             {liked && <LikeSVG />}
           </div>
-          <div className="flex w-full justify-evenly gap-9">
-            <div className="flex flex-col text-white items-center">
+          <div className="flex w-full justify-center">
+            <div className="flex flex-col mr-9 mt-3 text-white items-center">
               {liked ? (
                 <FcLike
                   onClick={handleClick}
@@ -136,33 +125,36 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
               )}
               <small>{likes}</small>
             </div>
-            <div className="flex flex-col text-white items-center">
+            <div className="flex flex-col mr-9 mt-3 text-white items-center">
               <TbMessageCircle className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer" />
               <small>0</small>
             </div>
-            <div className="flex flex-col text-white items-center">
+            <div className="flex flex-col mr-9 mt-3 text-white items-center">
               <RiShare2Line className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer" />
               <small>0</small>
             </div>
-            {fav ? (
-              <BsBookmarkFill
-                onClick={handlefavorites}
-                className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer"
-              />
-            ) : (
-              <BsBookmark
-                onClick={handlefavorites}
-                className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer"
-              />
-            )}
+            <div className="flex flex-col mr-9 mt-3 text-white items-center">
+              {fav ? (
+                <BsBookmarkFill
+                  onClick={handlefavorites}
+                  className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer"
+                />
+              ) : (
+                <BsBookmark
+                  onClick={handlefavorites}
+                  className="text-white border-none p-2 w-9 h-9 font-bold text-4xl cursor-pointer"
+                />
+              )}
+              <small>0</small>
+            </div>
           </div>
         </div>
 
-        <div className="lg:block md:block lg:w-1/2 sm:block hidden flex-1 gap-5 pl-2 mt-4">
-          <div className="flex justify-between gap-x-64">
+        <div className="hidden lg:block md:block lg:w-1/2 sm:w-full flex-1 gap-5 pl-2 mt-4">
+          <div className="flex w-full justify-between">
             <div className="flex flex-col w-full">
               <div className="flex gap-2 items-center">
-                <Avatar_profile
+                <AvatarProfile
                   image={selectedPost.user.image}
                   username={selectedPost.user.username}
                 />
@@ -203,12 +195,11 @@ const PostModal = ({ isOpen, onClose, selectedPost }) => {
                 {formatDate(selectedPost.posted)}
               </h1>
             </div>
-            <BsThreeDots className="mr-3 text-4xl" />
+            <BsThreeDots className="mr-9 text-4xl cursor-pointer" />
           </div>
           <hr className="w-full mt-2" />
-
           <div className="flex justify-center items-center h-96">
-            <h5>No Comment Yet</h5>
+            <h5>No Comments Yet</h5>
           </div>
         </div>
       </ModalContent>
